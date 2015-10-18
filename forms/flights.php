56ee55eddcf254410bf2a7d4f584092b5825a23f
Gwartney <davidg@boundary.com>
2015-10-17 17:21:13 -0700

    checkpoint

--- forms/flights.php
+++ forms/flights.php
@@ -20,14 +20,9 @@
 </head>
 <body>
 
-<div class="container-fluid">
-    <div class="col-sm-1">
-        <img id="logo" src="/images/fly-by-night.png"/>
-    </div>
-    <div class="col-sm-3">
-        <h1>Fly By Night</h1>
-    </div>
-</div>
+<?php
+include 'mast_head.php';
+?>
 
 <div class="container-fluid">
     <div class="col-sm-8">
---
+++ forms/mast_head.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="col-sm-1">
+        <img id="logo" src="/images/fly-by-night.png"/>
+    </div>
+    <div class="col-sm-3">
+        <h1>Fly By Night</h1>
+    </div>
+</div>
+
