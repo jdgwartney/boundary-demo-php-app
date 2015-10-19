4e503bdcb8d69bdb254457811baff6ca8ba32553
Gwartney <davidg@boundary.com>
2015-10-19 06:13:37 -0700

    Define flights table

--- api/db/configure.sql
+++ api/db/configure.sql
@@ -1,15 +1,17 @@
-DROP DATABASE IF EXISTS flybynight;
-DROP USER 'flybynight'@'localhost';
+-- DROP DATABASE IF EXISTS flybynight;
+DROP USER 'flybynight'@'%';
 
-CREATE DATABASE flybynight
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
+-- CREATE DATABASE flybynight
+-- DEFAULT CHARACTER SET utf8
+-- DEFAULT COLLATE utf8_general_ci;
 
-CREATE USER 'flybynight'@'localhost' IDENTIFIED BY 'fly123';
+CREATE USER 'flybynight'@'%' IDENTIFIED BY 'fly123';
 
-GRANT ALL ON flybynight.* TO 'flybynight'@'localhost';
+GRANT ALL ON flybynight.* TO 'flybynight'@'%';
 
 USE flybynight;
+
+DROP TABLE IF EXISTS foo;
 
 CREATE TABLE foo
 (
@@ -29,3 +31,18 @@ INSERT INTO foo(name)
 VALUES('green');
 
 SELECT * FROM foo;
+
+DROP TABLE IF EXISTS flights;
+
+CREATE TABLE flights
+(
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  airline VARCHAR(32),
+  flight VARCHAR(32),
+  from_airport VARCHAR(32),
+  to_airport VARCHAR(32),
+  departure DATETIME,
+  arrival DATETIME,
+  fltime VARCHAR(32),
+  PRIMARY KEY (id)
+);
