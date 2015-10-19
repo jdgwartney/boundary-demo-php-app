76affd94e4a5b2bc4f0e4821e08771e72f57273c
Gwartney <davidg@boundary.com>
2015-10-19 01:22:28 -0700

    Fix landing page

--- index.php
+++ index.php
@@ -14,25 +14,27 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="<?php
-include 'mast_head.php';
-?>https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body id="landing-body">
+<body>
 
 <?php
 include 'forms/mast_head.php';
 ?>
 <div class="container-fluid">
-    <div class="col-sm-6">
-    <p class="blockquote-reverse">Cheapest fares on the net...if you can fly at night</p>
-    </div>
-    <button class ="btn btn-link" type="button" role="link" >Sign Up</button>
+    <div class="panel panel-default">
+        <div class ="panel-body">
+            Cheapest fares on the net...if you can fly at night.
+        </div >
+    </div >
+</div>
+<div class="container-fluid">
     <div class="col-sm-3">
-        <button type="button">Button</button>
-        <input type="button" value ="Input" >
+        <div id="browse_app">
+            <a class="btn btn-large" href="/forms/flights.php">Search For Fares</a>
+        </div>
     </div>
 </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
